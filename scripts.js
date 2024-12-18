de3419570b4f57a8b5f292390529cf01b29ab318
abbas1221-oops <draftcrafter1@gmail.com>
2024-12-18 15:31:30 +0500

    FRee fire
    this is very best game

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Function to change the game description when the button is clicked
+function changeDescription() {
+    const newDescription = "In this reimagined version, battle across exotic, futuristic locations with enhanced AI, high-tech weapons, and a deadly range of enemies!";
+    document.getElementById('game-description').textContent = newDescription;
+}
